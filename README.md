d5d991a960577be00cc15423943e269161704ae5
Ryan Boudreaux <boudreauxryan@gmail.com>
2026-05-14 10:03:03 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# shot-term
-a minimal termainal without the bells and whistles
+# webterm
+
+Because I wanted a terminal app neither sang nor danced.
+
+## Building
+
+Currently only supports macOS, but it shouldn't take much to bring it elsewhere.
+
+Universal builds do not work due to packaging problems.
---
+++ forge.config.ts
@@ -0,0 +1,33 @@
+import { MakerZIP } from "@electron-forge/maker-zip";
+import { VitePlugin } from "@electron-forge/plugin-vite";
+import type { ForgeConfig } from "@electron-forge/shared-types";
+
+const config: ForgeConfig = {
+  packagerConfig: {
+    icon: "icon/webterm",
+  },
+  rebuildConfig: {},
+  makers: [new MakerZIP({}, ["darwin"])],
+  plugins: [
+    new VitePlugin({
+      build: [
+        {
+          entry: "src/main.ts",
+          config: "vite.main.config.ts",
+        },
+        {
+          entry: "src/preload.ts",
+          config: "vite.preload.config.ts",
+        },
+      ],
+      renderer: [
+        {
+          name: "main_window",
+          config: "vite.renderer.config.ts",
+        },
+      ],
+    }),
+  ],
+};
+
+export default config;
